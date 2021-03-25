8a547b8ae5545bd062bd08efdd01b5e20e1b2805
Francois Jacquier <fjacquier@gmail.com>
2021-03-25 16:03:40 +0100

    request offline sw

--- serviceworker.js
+++ serviceworker.js
@@ -89,16 +89,16 @@ const clearOldCaches = () =>
 
 addEventListener('install', (installEvent) => {
   installEvent.waitUntil(
-    //(async () => {
-    //const cache = await caches.open(cacheName)
-    // Setting {cache: 'reload'} in the new request will ensure that the
-    // response isn't fulfilled from the HTTP cache; i.e., it will be from
-    // the network.
-    //await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }))
-    updateCache()
-      .then(() => doCacheThings())
-      .then(() => skipWaiting())
-    //})()
+    (async () => {
+      const cache = await caches.open(cacheName)
+      // Setting {cache: 'reload'} in the new request will ensure that the
+      // response isn't fulfilled from the HTTP cache; i.e., it will be from
+      // the network.
+      await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }))
+      updateCache()
+        .then(() => doCacheThings())
+        .then(() => skipWaiting())
+    })()
   )
 })
 
